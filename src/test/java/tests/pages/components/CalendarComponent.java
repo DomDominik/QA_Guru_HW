c5df22949a1c1387d35bcd25e17390a32bc59ebf
DonDominik <shevchikdominik@gmail.com>
2026-03-25 23:20:48 +0300

    внес правки в календарь

--- src/test/java/tests/pages/components/CalendarComponent.java
+++ src/test/java/tests/pages/components/CalendarComponent.java
@@ -6,7 +6,7 @@ public class CalendarComponent {
     public void setDate(String month, String year, String day){
         $(".react-datepicker__month-select").selectOption(month);// Выбираем месяц
         $(".react-datepicker__year-select").selectOption(year);// Выбираем год
-        $(".react-datepicker__day.react-datepicker__day--005").click();// Выбираем день
+        $(".react-datepicker__day.react-datepicker__day--0" + day + ":not(react-datepicker__day--outside-month)").click();// Выбираем день
 
     }
 }
